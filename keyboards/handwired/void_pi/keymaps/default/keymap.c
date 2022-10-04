bfc18ad487ebf073b3c2da97d8ee38f9ef20dc4c
hugin0 <131902+Hugin0@users.noreply.github.com>
2022-10-04 08:51:38 -0400

    initial commit of my keymaps

---
+++ keyboards/handwired/void_pi/keymaps/default/keymap.c
@@ -0,0 +1,25 @@
+#include QMK_KEYBOARD_H
+
+enum layers {
+  _QWERTY
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+     */
+    [_QWERTY] = LAYOUT_ortho_4x12(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L
+    )
+};
---
+++ keyboards/handwired/void_pi/rules.mk
@@ -0,0 +1,8 @@
+# This file intentionally left blank
+
+BOARD = GENERIC_RP_RP2040
+SERIAL_DRIVER = vendor
+WS2812_DRIVER = vendor
+
+LAYOUTS = ortho_4x12
+
